954dd10ecb6af1cb0ae27d8808b3f37c77cc408d
LuzJimena050712 <luzjimenarcegabriel@gmail.com>
2025-11-10 10:59:46 -0600

    Inicio de API y definición de Gramática

--- proyect/api/views.py
+++ proyect/api/views.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 
 @api_view(['POST'])
+
 def main(request):
+    
     return Response(
         {"mensaje":"Hola"},
         status=status.HTTP_200_OK
