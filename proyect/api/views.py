e4726a2181a11d3d1f94d91dcff80887a721f760
LuzJimena050712 <luzjimenarcegabriel@gmail.com>
2025-11-13 15:19:50 -0600

    Lógica final

--- proyect/api/views.py
+++ proyect/api/views.py
@@ -1,37 +1,35 @@
-import json
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework import status
+import json
 
-#Importamos nuestro método
-from .utills import run_code
+# Importamos nuestro metodo
+from .utils import run_code
 
 @api_view(['POST'])
-
 def main(request):
+    # Definimos el metodo de la peticion
     if request.method != 'POST':
-        return JsonResponse(
+        return JsonResponse( 
             {'code':''},
-            status=405   
+            status=405
         )
-    
+
     try:
-        #Parseamos el cuerpo de la petición con un Json
-        body = request.body.decorate('utf-8') if request.body else ''
+        # Parseamos el cuerpo de la peticion en un JSON
+        body=request.body.decode('utf-8') if request.body else ''
         data = json.loads(body) if body else {}
     except Exception:
         return JsonResponse(
             {'code':'Json invalido'},
             status=405
-        )
-    #Del Json obtenemos el que tenga 'text'
-    code = data.get('text','')
-
-    #Ejecutamos las instrucciones con el método que definimos
+            )
+    # Del Json obtenemos el que tenga 'text'
+    code=data.get('text','')
+    # Ejecutamos las instrucciones con el metodo que definimos
     output = run_code(code)
-    #Da un respuesta de tipo Json
-    return Response(
-        {"output":output},
-        status=status.HTTP_200_OK
+    # Da una respuesta de tipo JSON
+    return JsonResponse(
+        {"output":output}
     )
